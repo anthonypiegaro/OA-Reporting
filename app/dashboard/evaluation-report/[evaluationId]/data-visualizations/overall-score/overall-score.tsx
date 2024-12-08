7717d4e2de76f2ca878ca8da4ab0871f26956d83
Anthony Piegaro <84252391+anthonypiegaro@users.noreply.github.com>
2024-12-08 21:52:02 +0000

    Add overall-score component

--- app/dashboard/evaluation-report/[evaluationId]/data-visualizations/overall-score/overall-score.tsx
+++ app/dashboard/evaluation-report/[evaluationId]/data-visualizations/overall-score/overall-score.tsx
@@ -1,1 +1,114 @@
 "use client"
+
+import {
+    Label,
+    PolarGrid,
+    PolarRadiusAxis,
+    RadialBar,
+    RadialBarChart
+} from "recharts";
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle
+} from "@/components/ui/card";
+import { ChartConfig, ChartContainer } from "@/components/ui/chart";
+
+const chartData = [
+    { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+]
+
+const chartConfig = {
+    visitors: {
+      label: "Visitors",
+    },
+    safari: {
+      label: "Safari",
+      color: "hsl(var(--chart-2))",
+    },
+  } satisfies ChartConfig
+
+interface OverallScoreProps {
+    totalScoredAssessments: number,
+    totalPassingAssessments: number
+}
+
+export default function OverallScore({ totalScoredAssessments, totalPassingAssessments }: OverallScoreProps) {
+    const scoreInDeg = Math.floor((totalPassingAssessments / totalScoredAssessments) * 360);
+    const scoreInPercent = `${((totalPassingAssessments / totalScoredAssessments) * 100).toFixed(0)}%`;
+
+    return (
+        <Card className="flex flex-col">
+            <CardHeader className="items-center pb-0">
+                <CardTitle>Quick Summary</CardTitle>
+                <CardDescription>Overall performance on scored assessments</CardDescription>
+            </CardHeader>
+            <CardContent className="flex-1 pb-0">
+                <ChartContainer
+                    config={chartConfig}
+                    className="mx-auto aspect-square max-h-[250px]"
+                >
+                    <RadialBarChart
+                        data={chartData}
+                        startAngle={0}
+                        endAngle={scoreInDeg}
+                        innerRadius={80}
+                        outerRadius={110}
+                    >
+                        <PolarGrid
+                            gridType="circle"
+                            radialLines={false}
+                            stroke="none"
+                            className="first:fill-muted last:fill-background"
+                            polarRadius={[86, 74]}
+                        />
+                        <RadialBar dataKey="visitors" background cornerRadius={10} />
+                        <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
+                            <Label
+                                content={({ viewBox }) => {
+                                if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                                    return (
+                                    <text
+                                        x={viewBox.cx}
+                                        y={viewBox.cy}
+                                        textAnchor="middle"
+                                        dominantBaseline="middle"
+                                    >
+                                        <tspan
+                                        x={viewBox.cx}
+                                        y={viewBox.cy}
+                                        className="fill-foreground text-4xl font-bold"
+                                        >
+                                            {scoreInPercent}
+                                        </tspan>
+                                        <tspan
+                                        x={viewBox.cx}
+                                        y={(viewBox.cy || 0) + 24}
+                                        className="fill-muted-foreground"
+                                        >
+                                            Passed
+                                        </tspan>
+                                    </text>
+                                    )
+                                }
+                                }}
+                            />
+                        </PolarRadiusAxis>
+                    </RadialBarChart>
+                </ChartContainer>
+            </CardContent>
+            <CardFooter className="flex-col gap-2 text-sm">
+                <div className="flex items-center gap-2 font-medium leading-none">
+                Based on a total of {totalScoredAssessments} assessments
+                </div>
+                <div className="leading-none text-muted-foreground">
+                You passed {totalPassingAssessments} out of {totalScoredAssessments} scored assessments
+                </div>
+            </CardFooter>
+        </Card>
+    )
+}
--- app/dashboard/evaluation-report/[evaluationId]/page.tsx
+++ app/dashboard/evaluation-report/[evaluationId]/page.tsx
@@ -1,4 +1,5 @@
 import { redirect } from "next/navigation";
+import OverallScore from "./data-visualizations/overall-score/overall-score";
 import { isAllowedToView } from "./actions/is-allowed-to-view";
 import { getEvaluationReport } from "./actions/get-evaluation-report/get-evaluation-report";
 
@@ -9,11 +10,50 @@ export default async function Page({
 }) {
     const evaluationId = (await params).evaluationId;
     const isAuthorized = await isAllowedToView(evaluationId);
-    const evaluationReportData = await getEvaluationReport(evaluationId);
 
     if (!isAuthorized) {
         redirect("/dashboard")
     }
 
-    return <div>{JSON.stringify(evaluationReportData)}</div>
+    const evaluationReportData = await getEvaluationReport(evaluationId);
+
+    const { totalScoredAssessments, totalPassingAssessments} = evaluationReportData.assessments.reduce((acc, assessment) => {
+        if (assessment.type === "quantitative" || assessment.type === "qualitative") {
+            acc.totalScoredAssessments += 1;
+
+            if (assessment.isPassing) {
+                acc.totalPassingAssessments += 1;
+            }
+        }
+
+        return acc;
+    }, {
+        totalScoredAssessments: 0,
+        totalPassingAssessments: 0
+    });
+
+    return (
+        <div className="flex flex-1 flex-wrap flex-row justify-center items-start gap-x-4 gap-y-4 md:justify-start">
+            <OverallScore 
+                evaluationName={evaluationReportData.evaluationName}
+                totalPassingAssessments={totalPassingAssessments}
+                totalScoredAssessments={totalScoredAssessments}
+            />
+            <OverallScore 
+                evaluationName={evaluationReportData.evaluationName}
+                totalPassingAssessments={totalPassingAssessments}
+                totalScoredAssessments={totalScoredAssessments}
+            />
+            <OverallScore 
+                evaluationName={evaluationReportData.evaluationName}
+                totalPassingAssessments={totalPassingAssessments}
+                totalScoredAssessments={totalScoredAssessments}
+            />
+            <OverallScore 
+                evaluationName={evaluationReportData.evaluationName}
+                totalPassingAssessments={totalPassingAssessments}
+                totalScoredAssessments={totalScoredAssessments}
+            />
+        </div>
+    );
 }
