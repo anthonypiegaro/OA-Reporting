dde1cf49e822fbfae596b292cf635b4760783ce7
Anthony Piegaro <84252391+anthonypiegaro@users.noreply.github.com>
2024-12-14 05:19:32 +0000

    Update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,18 +7,11 @@ if (localEnv.error) {
     dotenv.config({ path: '.env' });
 }
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-console.log("NODE_ENV is set to production: ", isProduction);
-console.log("The node env is set to:", process.env.NODE_ENV);
-
 export default defineConfig({
   out: './drizzle',
   schema: './app/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: isProduction
-    ? process.env.POSTGRES_PRODUCTION_URL!
-    : process.env.POSTGRES_URL!,
+    url: process.env.POSTGRES_URL!,
   },
 });
