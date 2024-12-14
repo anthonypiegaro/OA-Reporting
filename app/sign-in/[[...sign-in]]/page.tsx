8509f55f63bc66408d9c0e92a297dfebe2ab9be2
Anthony Piegaro <84252391+anthonypiegaro@users.noreply.github.com>
2024-12-14 05:46:43 +0000

    Update sign in page

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
     return (
         <main className="flex flex-1 justify-center items-center">
-            <SignIn forceRedirectUrl={"/dashboard"} path="/sign-in"/>
+            <SignIn />
         </main>
     );
 }
